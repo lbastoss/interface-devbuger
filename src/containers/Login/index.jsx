7257f5fe4425caa770bc3d4058dc78ee8705f55c
lbastoss <bastosls17@gmail.com>
2025-02-26 19:40:06 -0300

    iniciando a tela de login e criando os estilos

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,9 +1,35 @@
-
+import { Button, Container, Form, InputContainer, LeftContainer, Link, RightContainer, Tittle } from './styles'
+import Logo from '../../assets/Logo.svg'
 
 export function Login() {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
+	return (
+		<Container>
+			<LeftContainer>
+				<img src={Logo} alt="logo-devburger" />
+			</LeftContainer>
+
+			<RightContainer>
+				<Tittle>
+					Olá, seja bem vindo ao <span>Dev Burguer!</span> Acesse com seu <span>Login e senha.</span>
+				</Tittle>
+
+				<Form>
+					<InputContainer>
+						<label>Email</label>
+						<input type="email" />
+					</InputContainer>
+
+					<InputContainer>
+						<label>Senha</label>
+						<input type="password" />
+					</InputContainer>
+
+					<Link>Esqueci minha senha.</Link>
+					<Button>Entrar</Button>
+				</Form>
+
+				<Link>Não possui conta? Clique aqui.</Link>
+			</RightContainer>
+		</Container>
+	)
 }
---
+++ src/containers/Login/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    
+`
+export const LeftContainer = styled.div``
+
+export const RightContainer = styled.div``
+
+export const Tittle = styled.h2``
+
+export const Form = styled.form``
+
+export const InputContainer = styled.div``
+
+export const Link = styled.a``
+
+export const Button = styled.button``
