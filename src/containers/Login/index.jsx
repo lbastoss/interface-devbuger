7ed73aab64c59a9d5dfae563d78e9d510cc34b80
lbastoss <bastosls17@gmail.com>
2025-02-27 13:10:24 -0300

    algumas configs do biome e também começando as estilizações do login

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,35 +1,47 @@
-import { Button, Container, Form, InputContainer, LeftContainer, Link, RightContainer, Tittle } from './styles'
+import {
+  Button,
+  Container,
+  Form,
+  InputContainer,
+  LeftContainer,
+  RightContainer,
+  Tittle,
+} from './styles'
 import Logo from '../../assets/Logo.svg'
 
 export function Login() {
-	return (
-		<Container>
-			<LeftContainer>
-				<img src={Logo} alt="logo-devburger" />
-			</LeftContainer>
+  return (
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt="logo-devburger" />
+      </LeftContainer>
 
-			<RightContainer>
-				<Tittle>
-					Olá, seja bem vindo ao <span>Dev Burguer!</span> Acesse com seu <span>Login e senha.</span>
-				</Tittle>
+      <RightContainer>
+        <Tittle>
+          Olá, seja bem vindo ao <span>Dev Burguer!</span>
+          <br />
+          Acesse com seu
+          <span> Login e senha.</span>
+        </Tittle>
 
-				<Form>
-					<InputContainer>
-						<label>Email</label>
-						<input type="email" />
-					</InputContainer>
+        <Form>
+          <InputContainer>
+            <label>Email</label>
+            <input type="email" />
+          </InputContainer>
 
-					<InputContainer>
-						<label>Senha</label>
-						<input type="password" />
-					</InputContainer>
+          <InputContainer>
+            <label>Senha</label>
+            <input type="password" />
+          </InputContainer>
 
-					<Link>Esqueci minha senha.</Link>
-					<Button>Entrar</Button>
-				</Form>
+          <Button>Entrar</Button>
+        </Form>
 
-				<Link>Não possui conta? Clique aqui.</Link>
-			</RightContainer>
-		</Container>
-	)
+        <p>
+          Não possui conta? <a>Clique aqui.</a>
+        </p>
+      </RightContainer>
+    </Container>
+  )
 }
