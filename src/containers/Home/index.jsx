d4c0dd14e5858594039a28c4e6297a6e9c73ca3d
lbastoss <bastosls17@gmail.com>
2025-04-10 11:46:52 -0300

    iniciando a criação da tela de Home

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,7 +1,17 @@
+import { Banner, Content, Container } from './styles'
+
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main>
+      <Banner>
+        <h1>Bem-vindo(a)!</h1>
+      </Banner>
+      <Container>
+        <Content>
+          <div>Carrossel Cateogiras</div>
+          <div>Carrossel Produtos</div>
+        </Content>
+      </Container>
+    </main>
   )
 }
