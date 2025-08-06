26ac596ad3aa5bcaf3ce90064eb3c0852c4b5fa7
lbastoss <lucas.bastos.7@hotmail.com>
2025-08-06 16:42:04 -0300

    ajustando active do menu

--- src/components/SideNavAdmin/index.jsx
+++ src/components/SideNavAdmin/index.jsx
@@ -4,16 +4,22 @@ import Logo from '../../assets/logo.svg';
 import { UseUser } from '../../hooks/UserContext';
 import { navLinks } from './navLinks';
 import { Container, NavLinkContainer, NavLink, Footer } from './styles';
+import { useResolvedPath } from 'react-router-dom';
 
 export function SideNavAdmin() {
 	const { logout } = UseUser();
+	const { pathname } = useResolvedPath();
 
 	return (
 		<Container>
 			<img src={Logo} alt="Hamburguer logo DevBurger" />
 			<NavLinkContainer>
 				{navLinks.map((link) => (
-					<NavLink key={link.id} to={link.path}>
+					<NavLink
+						key={link.id}
+						to={link.path}
+						$isActive={pathname === link.path}
+					>
 						{link.icon}
 						<span>{link.label}</span>
 					</NavLink>
--- src/components/SideNavAdmin/styles.js
+++ src/components/SideNavAdmin/styles.js
@@ -28,9 +28,10 @@ export const NavLink = styled(Link)`
     padding: 12px 20px;
     text-decoration: none;
     color: ${(props) => props.theme.white};
+    background-color: ${(props) => (props.$isActive ? props.theme.purple : 'transparent')};
 
     &:hover {
-        background-color: ${(props) => props.theme.purple};
+        opacity: 0.9;
     }
 `;
 
