608fc011e852673006918ad7a326f988501b70dd
lbastoss <lucas.bastos.7@hotmail.com>
2025-08-13 17:05:36 -0300

    criando tela para adicionar produtos

--- src/containers/Admin/NewProduct/index.jsx
+++ src/containers/Admin/NewProduct/index.jsx
@@ -1,7 +1,63 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { ImageIcon } from '@phosphor-icons/react';
+
+import {
+	Container,
+	Form,
+	InputGroup,
+	Label,
+	Input,
+	LabelUpload,
+	Select,
+	SubmitButton,
+} from './styles';
+
+const schema = yup
+	.object({
+		firstName: yup.string().required(),
+		age: yup.number().positive().integer().required(),
+	})
+	.required();
+
 export function NewProduct() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(schema),
+	});
+	const onSubmit = (data) => console.log(data);
+
 	return (
-		<div>
-			<h2>Criar Produto</h2>
-		</div>
+		<Container>
+			<Form>
+				<InputGroup>
+					<Label>Nome</Label>
+					<Input />
+				</InputGroup>
+
+				<InputGroup>
+					<Label>Preço</Label>
+					<Input />
+				</InputGroup>
+
+				<InputGroup>
+					<LabelUpload>
+						<ImageIcon />
+						<input type="file" />
+					</LabelUpload>
+				</InputGroup>
+
+				<InputGroup>
+					<Label>Categoria</Label>
+					<Select />
+				</InputGroup>
+
+				<SubmitButton>Adicionar produto</SubmitButton>
+			</Form>
+		</Container>
 	);
 }
